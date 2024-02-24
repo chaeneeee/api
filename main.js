b32da8ff6457835af8b05f1cc9bd56943d84bebf
chaeyeon <cheche0804@naver.com>
2024-02-24 19:37:49 +0900

    new

--- main.js
+++ main.js
@@ -10,18 +10,28 @@ menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(eve
 console.log("ggg", menus)
 
 let url = new URL(
-    `https://apissss.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`)
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+
+let totalResults = 0
+let page = 1
+const pageSize = 10 // 페이지사이즈랑 그룹사이즈는 어차피 바꾸지않을거니까 const로 부여 
+const groupSize = 5
 
 const getNews = async() => {
     try {
-        const response = await fetch(url);
+        url.searchParams.set("page",page) //페이지라는 파라미터를 뒤에 붙여준다 그 값은  page 다  >> &page=page 로 볼 수 있음
+        url.searchParams.set("pageSize",pageSize)  //url 호출하기전에 붙인후에 url 호출해야한다.
+        const response = await fetch(url); //url에 페이지가 어딘지 &page 로 하나하나 값주면 힘드니 새롭게 url page함수 만들기
         const data = await response.json();
+        console.log("ddd", data);
         if( response.status=== 200){
                 if(data.articles.length===0){ //기사 길이가 0이니 아무것도 안 뜬 것 
                         throw new Error("No result for this search")
                 }
             newsList = data.articles;
+            totalResults = data.totalResults
             render();
+            paginationRender(); //페이지는 뉴스 다 나오고나서 나와야하니까 여기에다 호출 
         } else {
             throw new Error(data.message);
         }
@@ -55,7 +65,7 @@ const closeNav = () => {
 
 const getLatestNews = async () => {
     url = new URL(
-        `https://apissss.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
     
     );
 
@@ -64,14 +74,15 @@ const getLatestNews = async () => {
 
     
 }   
-getLatestNews();
+
 
 const getNewsByCategory= async (event)=>{
     // 유저가 무슨 카테고리를 눌렀는지 알기위해 만드는 것 밑에 줄 
     const category = event.target.textContent.toLowerCase(); //클릭이벤트시 카테고리를 소문자로 만들어줌 컴퓨터는 소문자 대문자 구별 꼭 해야해서 직접 바꾸기보단 클릭 후 바로 바뀌게 이 함수 쓴다. 
    
-    url = new URL(`https://apissss.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
     );
+
    getNews();
 
      //여기서 랜더는 위에 만들어준 뉴스리스트를 보여주는것 
@@ -81,7 +92,7 @@ const getNewsByCategory= async (event)=>{
 
 const getNewsByKeyword = async (event)=> {
     const keyword = document.getElementById("search-input").value;
-    url = new URL (`https://apissss.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
+    url = new URL (`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
     getNews();
 
 }
@@ -170,6 +181,52 @@ const openSearchBox = () => {
     }
   };
 openSearchBox()
+
+//페이지네이션만드는 함수
+const paginationRender = () => {
+    //totalresults
+    //page
+    //pageSize
+    //groupsize
+    //totalpages
+    const totalPages = Math.ceil(totalResults/pageSize)
+    //pagegroup
+    const pageGroup = Math.ceil(page/groupSize); // Math.ceil 은 나머지 숫자 올림해주느 것
+
+    //lastPage
+    let lastPage = pageGroup * groupSize;
+    //마지막 페이지 그룹이 그룹사이즈보다 작다? 4개 페이지만 보여줘야지. lastpage = totalpage
+    //firstPage
+    if(lastPage>totalPages){
+        lastPage = totalPages;
+    }
+    let firstPage = lastPage-(groupSize-1) <=0 ? 1 : lastPage-(groupSize-1)  // <=0보다 작거느나 같으면 1로 출력 그게 아니면 그대로 출력 
+        
+
+    let paginationHTML=``;
+    for(let i =firstPage; i<= lastPage; i++ ){
+        paginationHTML+=`<li class="page-item ${
+            i===page?"active":""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+    }    //i가 내가 지금보고있는페이지다? 그럼 active로 표시 아니면 말고 "" 빈칸 
+// 1부터 끝까지 page를 만드는거지 배열을 만드는것이 아니다 그래서 배열함수 못쓴다 for문을 써준다
+
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+    // <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    //       <li class="page-item"><a class="page-link" href="#">1</a></li>
+    //       <li class="page-item"><a class="page-link" href="#">2</a></li>
+    //       <li class="page-item"><a class="page-link" href="#">3</a></li>
+    //       <li class="page-item"><a class="page-link" href="#">Next</a></li>
+
+}
+
+const moveToPage=(pageNum) =>{
+    // console.log("movetopage",pageNum) 몇 번째 페에지 클릭했는 지 확인하는 것 
+    page=pageNum //위에서 페이지 1페이지로 let 해놨는데 여기선 이제 유동적으로 원하는 페이지를 로드해야함
+    getNews();
+
+
+}
+getLatestNews();
 
 
 // none 과 inline 바뀌도 ok 
